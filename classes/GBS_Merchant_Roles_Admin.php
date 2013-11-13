ba4b1305a2b45f09b2b5660e7af0956d3f4b3a2d
Dan Cameron <dan@sproutventure.com>
2013-11-12 16:55:44 -0800

    New Meta Box

--- classes/GBS_Merchant_Roles_Admin.php
+++ classes/GBS_Merchant_Roles_Admin.php
@@ -5,5 +5,11 @@
 */
 class GBS_Merchant_Roles_Admin extends Group_Buying_Controller {
 	
+	public static function init() {
+		// Add new options
+		add_filter( 'group_buying_template_meta_boxes/merchant-authorized-users.php', array( __CLASS__, 'replace_merchant_authorized_users_metabox'), 10, 1 );
+		add_filter( 'load_view_args_meta_boxes/merchant-authorized-users.php', array( __CLASS__, 'filter_merchant_authorized_users_metabox_args'), 10, 1 );
+
+	}
 	
 }
