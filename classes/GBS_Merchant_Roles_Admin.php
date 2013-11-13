39b486a09e273c3362f122f93f6bb928ac81b3ae
Dan Cameron <dan@sproutventure.com>
2013-11-12 16:56:21 -0800

    Meta box and args.

--- classes/GBS_Merchant_Roles_Admin.php
+++ classes/GBS_Merchant_Roles_Admin.php
@@ -10,6 +10,20 @@ class GBS_Merchant_Roles_Admin extends Group_Buying_Controller {
 		add_filter( 'group_buying_template_meta_boxes/merchant-authorized-users.php', array( __CLASS__, 'replace_merchant_authorized_users_metabox'), 10, 1 );
 		add_filter( 'load_view_args_meta_boxes/merchant-authorized-users.php', array( __CLASS__, 'filter_merchant_authorized_users_metabox_args'), 10, 1 );
 
+		// Save post
+		add_action( 'save_post', array( get_class(), 'maybe_save_user_role' ), 10, 2 );
+	}
+
+	public function replace_merchant_authorized_users_metabox( $file ) {
+		return GB_MERCHANT_ROLES_PATH . '/views/merchant-authorized-users.php';
+	}
+
+	public function filter_merchant_authorized_users_metabox_args( $args ) {
+		$args['merchant_user_role_option_name'] = self::OPTION_NAME;
+		$args['merchant_user_roles'] = GBS_Merchant_Role_Controller::get_roles();
+		return $args;
+	}
+
 	}
 	
 }
